fa037fcc053d24c5f2767cd3a906f1442fb8549b
Stefan <shodgeskluck@gmail.com>
2019-02-06 02:04:03 -0500

    Updates

---
+++ precache-manifest.d7957493f9103744f698b3ae47028f11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "f447fadb9ee547cb0a48",
+    "url": "/pomodoro-clock/static/js/main.f447fadb.chunk.js"
+  },
+  {
+    "revision": "3e2ae51d15731ea0bb59",
+    "url": "/pomodoro-clock/static/js/1.3e2ae51d.chunk.js"
+  },
+  {
+    "revision": "f447fadb9ee547cb0a48",
+    "url": "/pomodoro-clock/static/css/main.89867670.chunk.css"
+  },
+  {
+    "revision": "4546833c9242365cc031f09020855d06",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
